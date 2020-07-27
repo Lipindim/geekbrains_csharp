97a001020f187a0584ddbf6df0dd65b8271e525b
lipindim <lipindim@gmail.com>
2020-07-27 10:56:34 +1000

    Домашняя работа, урок 1

---
+++ HomeWork1/HomeWork.Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1.Task1
+{
+    /* Липин Дмитрий
+    Задача 1.    
+    Написать программу «Анкета». Последовательно задаются вопросы(имя, фамилия, возраст, рост, вес). В результате вся информация выводится в одну строчку:
+    а) используя склеивание;
+    б) используя форматированный вывод;
+    в) используя вывод со знаком $. */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите своё имя:");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Введите свою фамилию:");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Введите свой возраст:");
+            string age = Console.ReadLine();
+            Console.WriteLine("Введите свой рост:");
+            string height = Console.ReadLine();
+            Console.WriteLine("Введите свой вес:");
+            string weight = Console.ReadLine();
+
+            Console.WriteLine("Результат с использованием склеивания:");
+            Console.WriteLine(lastName + " " + firstName + ", ваш возраст: " + age + ", рост: " + height + ", вес: " + weight);
+            Console.WriteLine("Результат с использованием форматированного вывода:");
+            Console.WriteLine("{0} {1}, ваш возраст: {2}, рост: {3}, вес: {4}", lastName, firstName, age, height, weight);
+            Console.WriteLine("Использую вывод со знаком $");
+            Console.WriteLine($"{lastName} {firstName}, ваш возраст: {age}, рост: {height}, вес: {weight}");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ HomeWork1/HomeWork1.Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿using HomeWork.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1.Task2
+{
+    /* Липин Дмитрий
+     Задание 2
+     Ввести вес и рост человека.Рассчитать и вывести индекс массы тела(ИМТ) по формуле I=m/(h* h); где m — масса тела в килограммах, h — рост в метрах. */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите свой рост, см (целое число): ");
+            uint height = ExtraConsole.ReadUintFromConsole();
+            Console.WriteLine("Введите свой вес, кг (целое число): ");
+            uint weight = ExtraConsole.ReadUintFromConsole();
+            double bodyMassIndex = weight / (Math.Pow(height / 100d, 2));
+            Console.WriteLine($"Ваш индекс массы тела: {bodyMassIndex:f2}");
+            Console.ReadKey();
+
+        }
+    }
+}
